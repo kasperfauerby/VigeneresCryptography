1ea56260194df0111b9f07ec7bcabc6640470b16
kasper <kasperkbf@hotmail.com>
2021-02-16 12:30:04 +0100

    chart 2d array

--- src/demo/domain/Chart.java
+++ src/demo/domain/Chart.java
@@ -2,8 +2,25 @@ package demo.domain;
 
 public class Chart {
 
-    char chart[][] = {{'b', 'c', 'd', 'e', 'f','g'}};
+    char array[][] = {{'b', 'c', 'd', 'e', 'f','g'}};
 
+
+    public void createChart(int x, int y, String letter){
+     char chart[][] = new char[x][y];
+     int index=0;
+        for (int i = 0; i < x; i++) {
+            chart[0][i]=letter.charAt(i);
+            index++;
+        }
+    }
+
+    public void printChart(char[][] chart){
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+
+            }
+        }
+    }
 
 
 
